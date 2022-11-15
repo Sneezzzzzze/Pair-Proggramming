bef70fa1489946a60359b4597994b935219cf4e3
Laughingegg <112687944+Sneezzzzzze@users.noreply.github.com>
2022-11-15 09:49:33 +0700

    push

--- 1.0/4 November/BloodDonation.py
+++ 1.0/4 November/BloodDonation.py
@@ -1,5 +1,13 @@
 """BloodDonation"""
 def main():
     """beom"""
-    print("hello")
+    age = int(input())
+    weight = int(input())
+    have_you_ever = int(input())
+    if age == 17 or age == 60 <= 70:
+        commit = input()
+    if age < 17 or weight < 45 or (have_you_ever == 0 and age <= 55):
+        print("No")
+    else:
+        print("Yes")
 main()
