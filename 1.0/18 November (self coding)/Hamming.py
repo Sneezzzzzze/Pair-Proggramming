c7e84926b005343f0f0559e2e1bac48e79a64640
Laughingegg <112687944+Sneezzzzzze@users.noreply.github.com>
2022-11-21 22:07:51 +0700

    push

--- 1.0/18 November (self coding)/Hamming.py
+++ 1.0/18 November (self coding)/Hamming.py
@@ -1,6 +1,13 @@
 """ Hamming"""
-def main():
+def main(inp1, inp2):
     """beom"""
-    inp1 = input()
-    inp2 = input()
-main()
+    lisst, lissst, count = [], [], 0
+    for i in inp1:
+        lisst.append(i)
+    for j in inp2:
+        lissst.append(j)
+    for k in range(len(inp1)):
+        if lisst[k] != lissst[k]:
+            count += 1
+    print(count)
+main(input(), input())
