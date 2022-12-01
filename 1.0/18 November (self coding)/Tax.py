4e2d3e4590791179f2f69b003a43a32f323079ee
Laughingegg <112687944+Sneezzzzzze@users.noreply.github.com>
2022-12-01 15:36:29 +0700

    push

--- 1.0/18 November (self coding)/Tax.py
+++ 1.0/18 November (self coding)/Tax.py
@@ -1,19 +1,18 @@
 """Tax"""
 def main():
-    """beom"""
+    """beom help by mean"""
     year = int(input())
     cc_car = int(input())
-    oil = {
-        1<= cc_car <= 600: 0.5,
-        601 <= cc_car <= 1800: 1.50,
-        cc_car > 1801: 4.00
-    }
-    discount = {
-        year < 6: year,
-        year == 6: 10/100,
-        year == 7: 20/100,
-        year == 8: 30/100,
-        year == 9: 40/100,
-        year == 10: 50/100
-    }
-    oil[cc_car]
+    year_dic = {6:10, 7:20, 8:30, 9:40}
+    if cc_car > 1801:
+        ans = (600 * 0.5) + ((1800 - 600) * 1.5) + ((cc_car - 1800) * 4)
+    elif 601 < cc_car <= 1800:
+        ans = (600 * 0.5)+((cc_car - 600) * 1.5)
+    else:
+        ans = (cc_car * 0.5)
+    if year in year_dic:
+        ans = ans - (ans*(year_dic[year]/100))
+    elif year >= 10:
+        ans /= 2
+    print("%.2f"%ans)
+main()
